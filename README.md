1b7530b87ad78ac355b1a68e5d31845bc2f782b2
yimei-wu <wuyimei60@gmail.com>
2024-05-07 12:03:31 +0200

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# React NASA API Project
+
+[APOD - A Picture of The Day](https://api.nasa.gov/)
+
+```
+GET
+https://api.nasa.gov/planetary/apod
+```
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import DateNavigation from "../components/DateNavigation";
+
+function App() {
+  return (
+    <>
+      <DateNavigation />
+    </>
+  );
+}
+
+export default App;
